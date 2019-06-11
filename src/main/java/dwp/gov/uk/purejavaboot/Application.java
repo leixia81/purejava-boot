978cfb70427a9da2d73046659c8551d85f3f5fc1
Lei Xia <lei.xia.personal@gmail.com>
2019-06-11 13:12:36 +0100

    Remove un-used import.

--- src/main/java/dwp/gov/uk/purejavaboot/Application.java
+++ src/main/java/dwp/gov/uk/purejavaboot/Application.java
@@ -2,7 +2,6 @@ package dwp.gov.uk.purejavaboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Application {
