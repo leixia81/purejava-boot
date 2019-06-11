8083bf340e10bdfa605ccdee083a372f52dfaffd
Lei Xia <lei.xia.personal@gmail.com>
2019-06-11 12:14:30 +0100

    Amend code to use constructor injection instead field

--- src/main/java/dwp/gov/uk/purejavaboot/controller/MessageController.java
+++ src/main/java/dwp/gov/uk/purejavaboot/controller/MessageController.java
@@ -11,13 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
 
-    @Autowired
-    @Qualifier(value = "defaultMessageService")
     private MessageService messageService;
+
+    @Autowired
+    public MessageController(@Qualifier(value = "defaultMessageService") MessageService messageService) {
+        this.messageService = messageService;
+    }
 
 
     @GetMapping(value = "/api/ping")
-    public @ResponseBody String ping() {
+    public @ResponseBody
+    String ping() {
         return this.messageService.echoMessage();
     }
 }
